fe4730e46de29973f144596dcc885bb7a2a4c52b
Your Name <you@example.com>
2023-01-27 15:51:19 +0100

    add styles to header

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
-            JANE ESPER
+            ADEMIBOWALE ADEGBOYE
           </p>
           <p className="font-playfair text-md text-yellow">
-            ©2022 ESPER. All Rights Reserved.
+            ©2023 ADEMICROWN. All Rights Reserved.
           </p>
         </div>
       </div>
